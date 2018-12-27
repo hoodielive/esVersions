ff8e34f0374e556612bc7fd5afdf4908ef165016
hoodielive <hoodielive@gmail.com>
2018-12-27 13:14:22 -0500

    insane obj lit

--- es6/object-literals/objlit.js
+++ es6/object-literals/objlit.js
@@ -1,10 +1,23 @@
 let name = 'Aru'; 
 let age = 25; 
+let ageField = "age"; 
 
 let obj = {
   name, 
-  age
+  age,
+  [ageField]: 28, // dynamic fields 
+  greet() {
+    return(`${this.name}, ${this.age}`)
+    
+  },
+  "greet me"() {
+    return('Has the gift of sight..'); 
+    
+  },
+  
 }; 
 
 console.log(obj); 
-
+console.log(obj.greet()); 
+console.log(obj["greet me"]()); 
+console.log(obj["age"]); // you can still access it with obj.age or obj[ageField]; 
