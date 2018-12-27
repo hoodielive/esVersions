d6a965f94c211c1985db68b5bb1cdca78327b338
hoodielive <hoodielive@gmail.com>
2018-12-27 13:21:27 -0500

    the rest operator . . .

---
+++ es6/rest-operators/restops.js
@@ -0,0 +1,9 @@
+
+function sumUp(...toAdd) {
+  let result = 0; 
+  for (let i = 0; i < toAdd.length; i++) {
+    result += toAdd[i];  
+  }
+  return result; 
+}
+console.log(sumUp(100, 10, "20")); 
