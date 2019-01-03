de2fcf85be8e07abc282cb2eea288b9a3fb0e037
hoodielive <hoodielive@gmail.com>
2019-01-03 11:11:02 -0500

    deep vs shallow merge

---
+++ tips-tricks/shallow-deep-merge.js
@@ -0,0 +1,10 @@
+/** 
+ * What is the difference between deep and shallow merge of objects in javascript?
+ * Does deep merge recursively copy all source object enumerable properties into a target object?
+ * What does shallow merge do?
+*/ 
+
+/** Regarding shallow merge, the properties of the first object are overwritten withe the properties of the second object */ 
+
+
+
