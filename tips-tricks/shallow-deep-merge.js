ceddf875311055024c94f05f8c07157d61782d04
hoodielive <hoodielive@gmail.com>
2019-01-03 11:23:38 -0500

    wrong wrong wrong

--- tips-tricks/shallow-deep-merge.js
+++ tips-tricks/shallow-deep-merge.js
@@ -6,5 +6,43 @@
 
 /** Regarding shallow merge, the properties of the first object are overwritten withe the properties of the second object */ 
 
+let object1 = {
+	vibe1: {
+		property1: 42,
+	},
+}; 
 
+let object2 = {
+	vibe2: {
+		property2: 43, 
+	}, 
+	newVibe: {
+		property3: 99,
+	}, 
+};
 
+// shallow 
+let resultShallow = {
+	resultVibration: {
+		property2: 83,
+	}, 
+	resultNewVibe: {
+		property3: 900,
+	},
+}; 
+
+// Deep 
+let resultDeep = {
+	resultVibeDeep: {
+		property1: 420,
+		property2: 2100, 
+	}, 
+	newVibrationDeep: {
+		property3: 10,
+	},
+}; 
+
+console.log(object1);
+console.log(object2);
+console.log(resultShallow);
+console.log(resultDeep);
